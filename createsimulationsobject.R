7355e147eea0ab6fd1a1df763118847a3b354ba9
dluzzi <damiano.luzzi@wur.nl>
2016-03-15 14:56:16 +0100

    Update creation of simulations object

--- createsimulationsobject.R
+++ createsimulationsobject.R
@@ -1,6 +1,6 @@
 ## Used for testing only
 ## Creates an obejct of class "simulation"
-## This is used as an input for the plotuncert function
+
 library(raster)
 
 # Create Class ------------------------------------------------------------
@@ -9,7 +9,10 @@ library(raster)
 setClass("Simulations", slots =
            c(Realisations="RasterBrick",
              Mean = "RasterLayer",
-             Standard.Deviation = "RasterLayer")
+             Standard.Deviation = "RasterLayer",
+             Most.Likely.Class = "RasterLayer",
+             Class.Probabilities = "RasterBrick",
+             Quantiles = "RasterBrick")
            )
 
 
@@ -20,7 +23,8 @@ zlatibor.brick <- brick()
 
 # Load data to create object
 for (i in 1:100){
-  input <- paste("/Users/damianoluzzi/Desktop/Scripting/spup/data/zlatibor_dem_simulations/DEMsim", i, ".asc", sep = "")
+  #input <- Insert directory
+  #input <- paste("D:/DamianoLuzzi-Thesis-DO-NOT-REMOVE/spup/data/zlatibor_dem_simulations/DEMsim", i, ".asc", sep = "")
   DEM <- raster(input)
   zlatibor.brick <- addLayer(zlatibor.brick, DEM)
 }
@@ -28,12 +32,14 @@ for (i in 1:100){
 # Convert stack to brick
 zlatibor.brick <- brick(zlatibor.brick)
 
-# Calculate mean and sd
+# Calculate mean, sd and quantiles
 std<-calc(zlatibor.brick, fun = sd, na.rm = T)
 mean <- mean(zlatibor.brick, na.rm = T)
+quantiles <- calc(zlatibor.brick, fun = function(x) {quantile(x,
+                    probs = c(.05,.25, .5, .75, .95),na.rm=TRUE)} )
 
 
 # Create object of class Simulations --------------------------------------
 
 simulations <- new("Simulations", Realisations = zlatibor.brick, Mean = mean,
-                   Standard.Deviation = std)
+                   Standard.Deviation = std, Quantiles = quantiles)
